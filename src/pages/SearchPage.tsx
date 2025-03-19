69d9954461b4f2064144d69e0e518790f744ea63
Chi Hao Ly <vuighe.net01@gmail.com>
2025-03-19 17:11:09 +0700

    add new loading

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -68,11 +68,24 @@ const SearchPage = () => {
   };
 
   if (isLoading) {
-    return <span>Loading...</span>;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-10 h-10 border-4 border-orange-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   }
 
   if (!results?.data || !city) {
-    return <span>No results found</span>;
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <p className="text-xl font-semibold text-orange-500">
+          No results found.
+        </p>
+        <p className="text-sm text-orange-400">
+          Please try again with a different place.
+        </p>
+      </div>
+    );
   }
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
